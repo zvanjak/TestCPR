754efd7aff51e40aa7e13b76c34bcb3659ae2dca
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2023-11-22 18:42:40 +0100

    tdaa

---
+++ cpr_with_NuGet_solution/cpr_with_vcpkg_solution/Test_vcpkg/Test_vcpkg.cpp
@@ -0,0 +1,11 @@
+// Test_vcpkg.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+#include <cpr/cpr.h>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
